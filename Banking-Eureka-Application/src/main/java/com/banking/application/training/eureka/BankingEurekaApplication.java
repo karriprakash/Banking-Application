47b28d23570cfb9f8b64a5279ff4380384bc915e
karriprakash <karriprakash342@gmail.com>
2023-03-26 18:32:15 +0530

    Initial Commit in Banking application

---
+++ Banking-Eureka-Application/src/main/java/com/banking/application/training/eureka/BankingEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.banking.application.training.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingEurekaApplication.class, args);
+	}
+
+}
---
+++ Banking-Eureka-Application/src/test/java/com/banking/application/training/eureka/BankingEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.application.training.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
