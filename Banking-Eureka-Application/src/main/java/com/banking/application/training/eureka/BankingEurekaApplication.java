01702fd097da75be3af331f43d9602a84fe67fa1
karriprakash <karriprakash342@gmail.com>
2023-03-30 01:15:17 +0530

    Fully Implemented Eureka Application

--- Banking-Eureka-Application/src/main/java/com/banking/application/training/eureka/BankingEurekaApplication.java
+++ Banking-Eureka-Application/src/main/java/com/banking/application/training/eureka/BankingEurekaApplication.java
@@ -2,8 +2,11 @@ package com.banking.application.training.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class BankingEurekaApplication {
 
 	public static void main(String[] args) {
